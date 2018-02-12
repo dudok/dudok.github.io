dbdb455e2fd5ca1313223d89df1722aa833b2dc0
Sander Dudok <sander.dudok@gmail.com>
2018-02-12 15:14:24 +0100

    gave it a cleaner look.

---
+++ script/designs.js
@@ -0,0 +1,9 @@
+function myMenuButton() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+};
+$('#title').fadeOut(8000);
