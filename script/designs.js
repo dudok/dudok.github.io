43f20a52d26e5d41f6f094300c882049f7352558
Sander Dudok <sander.dudok@gmail.com>
2018-02-18 22:11:47 +0100

    added randColor()

--- script/designs.js
+++ script/designs.js
@@ -1,4 +1,4 @@
-var boxes;
+
 
 function myMenuButton() {
     var x = document.getElementById("myTopnav");
@@ -9,40 +9,57 @@ function myMenuButton() {
     }
 };
 
-$('#title').fadeOut(8000);
+$('#title').fadeOut(5000);
 
-function appendBoxes(tag, side, size='3.5vw', color='#617B6F' ) {
-    boxes = $(side);
-    $(tag).appendTo(boxes).css({'font-size':size, 'color':color});
+function randColor(tag) {
+    rand = Math.random();
+    if ($(tag).hasClass('fa fa-square')) {
+        if (rand <= 0.05) {
+            color='#375235';//'#7CA26E';
+        } else if (rand > 0.05 && rand <= 0.1) {
+            color='#42633F';
+        }
+        
+        
+        else if (rand > 0.1 && rand <= 0.7) {
+            color='#617B6F';
+        } else {
+            color='#333333';
+        }
+    } else {
+        color='#617B6F';
+    }
+    return color
 }
 
+function appendBoxes(tag, side, size='3.5vw') {
+    
+    $(tag).appendTo($(side)).css({'font-size':size, 'color':randColor(tag)});
+}
 
-function createSquares(side) {
+function createBoxes(side) {
+    let tag;
     for (let i = 0; i <= 140; i++) {
         if (side==='#left') {
             switch (i) {
                 case 0:
-                    appendBoxes('<a href="https://nl.linkedin.com/in/sander-dudok-34675488" target="_blank"><i class="fa fa-linkedin-square"></i></a>', '#left');
+                    tag = '<a href="https://nl.linkedin.com/in/sander-dudok-34675488" target="_blank"><i class="fa fa-linkedin-square"></i></a>';
                     break;
                 case 1:
-                    appendBoxes('<a href="https://github.com/dudok" target="_blank"><i class="fa fa-github-square"></i></a>', "#left");
+                    tag = '<a href="https://github.com/dudok" target="_blank"><i class="fa fa-github-square"></i></a>';
                     break;
                 case 2:
-                    appendBoxes('<a href="#sander.dudok@gmail.com"><i class="fa fa-envelope-square"></i></a>', "#left");
+                    tag = '<a href="#sander.dudok@gmail.com"><i class="fa fa-envelope-square"></i></a>';
                     break;
                 default:  
-                    appendBoxes('<i class="fa fa-square"></i>', "#left"); 
+                    tag = '<i class="fa fa-square"></i>'; 
             }
         } else {
-            appendBoxes('<i class="fa fa-square"></i>', '#right');
+            tag = '<i class="fa fa-square"></i>';
         }
-    }
-    $('.boxes').children().each( function() {
-        if (Math.random() >=0.7 && $(this).hasClass('fa fa-square')) {
-            $(this).css("color", "#333");
-        }
-    });
-}
+        appendBoxes(tag, side);
+    };
+};
 
-createSquares('#left');
-createSquares('#right');
+createBoxes('#left');
+createBoxes('#right');
